b54666e503469ac06ca1144403091db700f9ae5f
Nevoit <sunanbang2007@qq.com>
2025-10-30 22:42:37 +0800

    feat: Add arm64-v8a ABI filter to NDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters.add("arm64-v8a")
+        }
     }
 
     buildTypes {
